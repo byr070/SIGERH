2b7d58778d4b13e7a9542ed2275a01bc69906f93
Daniel Tapia <danieltapiamoreno@gmail.com>
2015-06-01 07:53:21 -0500

    controlador cuadrillas

--- application/controllers/cuadrillas.php
+++ application/controllers/cuadrillas.php
@@ -1,5 +1,5 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-class Cargos extends CI_Controller {
+class Cuadrillas extends CI_Controller {
 	
 	private $id_modulo = NULL;
 	
@@ -11,7 +11,6 @@ class Cargos extends CI_Controller {
 		$this->load->library('tank_auth_groups','','tank_auth');
 		$this->load->library('table');
 		$this->lang->load('tank_auth','spanish');
-		//$this->load->model('empleado/empleado_model');
 		$this->load->model('catalogos/modulos_model');
 		$this->id_modulo = $this->modulos_model->get_id_modulo_por_nombre(get_class($this));
 	}
@@ -20,62 +19,56 @@ class Cargos extends CI_Controller {
         if (!$this->tank_auth->is_logged_in()) {
             redirect('/auth/login/');
         } else {
-        	//if(!is_null($this->id_modulo)){
-                redirect('/cargos/listar/');
-            //} else {
-            //	redirect('/inicio/');
-            //}
+        	if(!is_null($this->id_modulo)){
+                redirect('/cuadrillas/listar/');
+            } else {
+            	redirect('/inicio/');
+            }
         }
     }
 	
 	public function listar() {
-        //if(!is_null($this->id_modulo)){
-			$table_name='cargos';
+        if(!is_null($this->id_modulo)){
+			$table_name='cuadrillas';
 			$crud = new grocery_CRUD();
-    	    $crud->set_subject('Cargo');
+            $crud->set_theme('bootstrap');
+    	    $crud->set_subject('Cuadrillas');
     	    $crud->set_table($table_name)
-            ->columns('CRG_NOMBRE','CRG_DESCRIPCION','JORNADA_ID','HORARIO_ID','CRG_CREADO','CRG_MODIFICADO')
-            ->fields('CRG_NOMBRE','CRG_DESCRIPCION','JORNADA_ID','HORARIO_ID')
-            ->display_as('CRG_NOMBRE','NOMBRE')
-            ->display_as('CRG_DESCRIPCION','DESCRIPCION')
-            ->display_as('JORNADA_ID','JORNADA')
-            ->display_as('HORARIO_ID','HORARIO')
-            ->display_as('CRG_CREADO','CREADO')
-            ->display_as('CRG_MODIFICADO','CRG_MODIFICADO')
+            ->columns('CDR_NOMBRE','PROYECTO_ID','JORNADA_ID','HORARIO_ID')
+            ->fields('CDR_NOMBRE','PROYECTO_ID','JORNADA_ID','HORARIO_ID')
 
+            ->display_as('CDR_NOMBRE','Nombre de la cuadrilla')
+            ->display_as('PROYECTO_ID','Proyecto')
+            ->display_as('JORNADA_ID','Jornada')
+            ->display_as('HORARIO_ID','Horario')
+            
+            ->set_relation('PROYECTO_ID','proyectos','PRY_NOMBRE')
             ->set_relation('JORNADA_ID','jornadas','{JRN_DIAS_TRABAJO} - {JRN_DIAS_DESCANSO}')
             ->set_relation('HORARIO_ID','horarios','De {HRR_HORA_INICIO} a {HRR_HORA_FIN}')
-            
-            ->set_rules('CRG_NOMBRE','Nombre del cargo','required')
-            ->set_rules('CRG_DESCRIPCION','Descripción','required')
-            ->set_rules('JORNADA_ID','Jornada','required')
-            ->set_rules('HORARIO_ID','Horario','required');
+
+            ->set_rules('CDR_NOMBRE','required|is_unique[cuadrillas.cdr_nombre]|max_length[60]')
+            ->set_rules('CRG_NOMBRE','Nombre del cargo','required');
 
             //leer permisos desde la bd
-            //$arr_acciones = $this->modulos_model->get_acciones_por_rol_modulo($this->tank_auth->is_admin(), $this->id_modulo[0]);
-            //deshabilitar opciones unset_read,unset_edit,unset_delete,unset_add
-            //print_r($arr_acciones);
-            // $crud->unset_operations();
-            //Ocultar botón Ver, Exportar, Imprimir
-            $crud->unset_read();
+            $arr_acciones = $this->modulos_model->get_acciones_por_rol_modulo($this->tank_auth->is_admin(), $this->id_modulo[0]);
             $crud->unset_export();
             $crud->unset_print();
             //si no tiene permiso para add entonces
-            /*if(!in_array('Crear', $arr_acciones)) {
+            if(!in_array('Crear', $arr_acciones)) {
                 $crud->unset_add();
-            }*/
+            }
             //si no tiene permiso para editar entonces
-            /*if(!in_array('Editar', $arr_acciones)) {
+            if(!in_array('Editar', $arr_acciones)) {
                 $crud->unset_edit();
-            }*/
+            }
             //si no tiene permiso para leer entonces
-            /*if(!in_array('Ver', $arr_acciones)) {
+            if(!in_array('Ver', $arr_acciones)) {
                 $crud->unset_list();
-            }*/
+            }
             //si no tiene permiso para borrar entonces
-            /*if(!in_array('Eliminar', $arr_acciones)) {
+            if(!in_array('Eliminar', $arr_acciones)) {
                 $crud->unset_delete();
-            }*/
+            }
             try {
                 $output = $crud->render();
             } catch(Exception $e) {
@@ -85,14 +78,14 @@ class Cargos extends CI_Controller {
                     show_error($e->getMessage());
                 }
             }
-	        $this->_periodo_output($output);
-        //} else {
-        //	redirect('/inicio/');
-        //}
+	        $this->_cargo_output($output);
+        } else {
+        	redirect('/inicio/');
+        }
     }
 
 
-    function _periodo_output($output = null) {
+    function _cargo_output($output = null) {
     	$data['user_id']    = $this->tank_auth->get_user_id();
         $data['username']   = $this->tank_auth->get_username();
         $data['is_admin']   = $this->tank_auth->is_admin();
