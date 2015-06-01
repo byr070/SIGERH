394cf464db02ffd117dab9ec7c226db98e5b1c7c
Daniel Tapia <danieltapiamoreno@gmail.com>
2015-06-01 08:12:06 -0500

    seguridades vacaciones

--- application/controllers/vacaciones.php
+++ application/controllers/vacaciones.php
@@ -20,18 +20,19 @@ class Vacaciones extends CI_Controller {
         if (!$this->tank_auth->is_logged_in()) {
             redirect('/auth/login/');
         } else {
-        	//if(!is_null($this->id_modulo)){
+        	if(!is_null($this->id_modulo)){
                 redirect('/vacaciones/listar/');
-            //} else {
-            //	redirect('/inicio/');
-            //}
+            } else {
+            	redirect('/inicio/');
+            }
         }
     }
 	
 	public function listar() {
-        //if(!is_null($this->id_modulo)){
+        if(!is_null($this->id_modulo)){
 			$table_name='vacaciones';
 			$crud = new grocery_CRUD();
+            // $crud->set_theme('bootstrap');
     	    $crud->set_subject('Vacaciones')
                  ->set_table($table_name)
                  ->columns('EMPLEADO_ID','VCC_FECHA_INICIO','VCC_FECHA_FIN','VCC_DIAS_DISPONIBLES')
@@ -47,30 +48,29 @@ class Vacaciones extends CI_Controller {
             $crud->set_rules('VCC_FECHA_INICIO','fecha inicio','required');
             $crud->set_rules('VCC_FECHA_FIN','fecha fin','required');
 	        //leer permisos desde la bd
-            //$arr_acciones = $this->modulos_model->get_acciones_por_rol_modulo($this->tank_auth->is_admin(), $this->id_modulo[0]);
+            $arr_acciones = $this->modulos_model->get_acciones_por_rol_modulo($this->tank_auth->is_admin(), $this->id_modulo[0]);
             //deshabilitar opciones unset_read,unset_edit,unset_delete,unset_add
             //print_r($arr_acciones);
             // $crud->unset_operations();
             //Ocultar botón Ver, Exportar, Imprimir
-            $crud->unset_read();
             $crud->unset_export();
             $crud->unset_print();
             //si no tiene permiso para add entonces
-            /*if(!in_array('Crear', $arr_acciones)) {
+            if(!in_array('Crear', $arr_acciones)) {
                 $crud->unset_add();
-            }*/
+            }
             //si no tiene permiso para editar entonces
-            /*if(!in_array('Editar', $arr_acciones)) {
+            if(!in_array('Editar', $arr_acciones)) {
                 $crud->unset_edit();
-            }*/
+            }
             //si no tiene permiso para leer entonces
-            /*if(!in_array('Ver', $arr_acciones)) {
+            if(!in_array('Ver', $arr_acciones)) {
                 $crud->unset_list();
-            }*/
+            }
             //si no tiene permiso para borrar entonces
-            /*if(!in_array('Eliminar', $arr_acciones)) {
+            if(!in_array('Eliminar', $arr_acciones)) {
                 $crud->unset_delete();
-            }*/
+            }
             try {
                 $output = $crud->render();
             } catch(Exception $e) {
@@ -81,9 +81,9 @@ class Vacaciones extends CI_Controller {
                 }
             }
 	        $this->_vacacion_output($output);
-        //} else {
-        //	redirect('/inicio/');
-        //}
+        } else {
+        	redirect('/inicio/');
+        }
     }
 
 
@@ -100,8 +100,6 @@ class Vacaciones extends CI_Controller {
         $output = array_merge($output,$menu);
         $this->load->view('template/template.php',$output);    
     }
-
-
 }
 
 /* End of file vacaciones.php */
