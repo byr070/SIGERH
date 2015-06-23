0f02515b8e9e1db8f4dde60e8c277343a292a18e
Daniel Tapia <danieltapiamoreno@gmail.com>
2015-06-23 01:29:24 -0500

    footer
    error en nav bar por orden o versiones de: jquery y bootstrap.min.js

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,22 +1,28 @@
-<style type="text/css">
-	.footer{
-		padding-left: 15px;
-		padding-right: 15px;
-	}
-</style>    <!-- Inicio Pie -->
-    <div class="footer">
-        <p style="float: left;">© 2015 SIGERH. Todos los derechos reservados.</p>
-        <p style="float: right;">Página mostrada en <strong>{elapsed_time}</strong> segundos.</p>
-    </div>
-    <!-- Fin Pie -->
-
-	<script type="text/javascript" src="<?php echo base_url("assets/js/jquery-1.11.2.js") ?>"></script>
-    <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.min.js") ?>"></script>
-    <script type="text/javascript" src="<?php echo base_url("assets/js/numericKeyboard.js") ?>"></script>
-    <script type="text/javascript">
-    $('#myModal').on('hidden.bs.modal', function (e) {
-        window.location = "<?php echo base_url("/anticipos") ?>"
-    })
-    </script>
-</body>
+       <!-- Inicio Pie -->
+        <div class="footer">
+            <p style="float: left;">© 2015 SIGERH. Todos los derechos reservados.</p>
+            <p style="float: right;">Página mostrada en <strong>{elapsed_time}</strong> segundos.</p>
+        </div>
+        <!-- Fin Pie -->
+   
+    	<script type="text/javascript" src="<?php echo base_url("assets/js/jquery-1.11.2.js") ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.min.js") ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url("assets/js/numericKeyboard.js") ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap-clockpicker.min.js') ?>"></script>
+        
+        <script type="text/javascript">
+            $('#myModal').on('hidden.bs.modal', function (e) {
+                window.location = "<?php echo base_url("/anticipos") ?>"
+            })
+            $('.clockpicker').clockpicker({
+                'default' :'now',
+                placement: 'right',
+                // donetext:'Aceptar',
+                autoclose:true,
+                // fromnow:1000,
+            }).find('input').change(function(){
+                console.log(this.value);
+            });
+        </script>
+    </body>
 </html>
